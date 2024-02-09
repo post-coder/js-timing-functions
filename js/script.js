f8b0ae5d028a082c4c3b0d4f70d17fd37776744d
post-coder <gabrielspanu.dev@gmail.com>
2024-02-09 12:59:49 +0100

    setInterval

--- js/script.js
+++ js/script.js
@@ -1,34 +1,29 @@
 
 
-console.log("primo log");
+// voglio scrivere ogni secondo la stringa "tick" in console
 
-
-// setTimeout NON BLOCCA l'esecuzione delle altre righe di codice
-// programmazione asincrona
-setTimeout(funzioneDaChiamare, 2000);
-// setTimeout ordina al browser di memorizzarsi in una specie di lista d'attesa una cosa:
-    // chiamare la funzione indicata (funzioneDaChiamare) dopo tot millisecondi
+// questa funzione verrà chiamata ogni tot ALL'INFINITO
+const tickContinuo = setInterval(tick, 1000);
 
 
 
+document.querySelector("button").addEventListener("click", function() {
 
-// se abbiamo bisogno di passare un parametro
-// dobbiamo dichiarare dentro il timeout una funzione anonima
-setTimeout(function() {
-    // chiamiamo la funzione che ci interessa al suo interno, passandole i parametri
-    secondaFunzione("Testo da stampare");
-}, 2000);
+    // blocchiamo il setInterval
+    // per bloccare un setInterval ci basta memorizzarlo inizialmente in una variabile
+    // e poi inserire quella variabile come argomento della funzione "clearInterval"
+    clearInterval(tickContinuo);
+
+});
 
 
+function tick() {
 
-// Funzioni
+    console.log("tick");
 
-function funzioneDaChiamare() {
-    console.log("Funzione chiamata nel timeout");
 }
 
-// questa funzione ha bisogno di un parametro text
-function secondaFunzione(text) {
 
-    console.log(text)
+function tock() {
+    console.log("tock");
 }
